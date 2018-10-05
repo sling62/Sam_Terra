c3d43581da005be70719081d175476b59ad7876f
samantha.ling <samantha.ling@accenture.com>
2018-10-05 15:05:52 +0100

    updated variables

--- provider.tf
+++ provider.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region                  = "eu-west-1"
-  shared_credentials_file = "/Users/samantha.ling/.aws/credentials"
+  #shared_credentials_file = "/Users/samantha.ling/.aws/credentials"
   profile                 = "terraform_workshop"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}" 
 }
